a5624d786d86c22e892b28bc060111126ced793d
Georgii Novoselov <georgii@vaticle.com>
2024-06-26 14:43:54 +0100

    Add cascade annotation

--- concept/type_/relation_type.rs
+++ concept/type_/relation_type.rs
@@ -27,7 +27,7 @@ use crate::{
     concept_iterator,
     error::{ConceptReadError, ConceptWriteError},
     type_::{
-        annotation::{Annotation, AnnotationAbstract},
+        annotation::{Annotation, AnnotationAbstract, AnnotationCascade},
         attribute_type::AttributeType,
         object_type::ObjectType,
         owns::Owns,
@@ -205,7 +205,10 @@ impl<'a> RelationType<'a> {
         match annotation {
             RelationTypeAnnotation::Abstract(_) => {
                 type_manager.set_annotation_abstract(snapshot, self.clone().into_owned())?
-            }
+            },
+            RelationTypeAnnotation::Cascade(_) => {
+                type_manager.set_annotation_abstract(snapshot, self.clone().into_owned())?
+            },
         };
         Ok(())
     }
@@ -219,7 +222,10 @@ impl<'a> RelationType<'a> {
         match annotation {
             RelationTypeAnnotation::Abstract(_) => {
                 type_manager.unset_owner_annotation_abstract(snapshot, self.clone().into_owned())?
-            }
+            },
+            RelationTypeAnnotation::Cascade(_) => {
+                type_manager.unset_annotation_abstract(snapshot, self.clone().into_owned())?
+            },
         }
         Ok(()) // TODO
     }
@@ -370,12 +376,14 @@ impl<'a> PlayerAPI<'a> for RelationType<'a> {
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum RelationTypeAnnotation {
     Abstract(AnnotationAbstract),
+    Cascade(AnnotationCascade),
 }
 
 impl From<Annotation> for RelationTypeAnnotation {
     fn from(annotation: Annotation) -> Self {
         match annotation {
             Annotation::Abstract(annotation) => RelationTypeAnnotation::Abstract(annotation),
+            Annotation::Cascade(annotation) => RelationTypeAnnotation::Cascade(annotation),
 
             Annotation::Distinct(_) => unreachable!("Distinct annotation not available for Relation type."),
             Annotation::Independent(_) => unreachable!("Independent annotation not available for Relation type."),
@@ -391,6 +399,7 @@ impl Into<Annotation> for RelationTypeAnnotation {
     fn into(self) -> Annotation {
         match self {
             RelationTypeAnnotation::Abstract(annotation) => Annotation::Abstract(annotation),
+            RelationTypeAnnotation::Cascade(annotation) => Annotation::Cascade(annotation),
         }
     }
 }
